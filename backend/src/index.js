bf24e61f130fa83c8dedb2ec99c57341459289f4
jackwarriors <24bcs098@iiitdwd.ac.in>
2026-07-18 13:33:28 +0530

    intial commit

---
+++ backend/src/index.js
@@ -0,0 +1,9 @@
+// const express = require("express")
+import express from "express";
+import "dotenv/config"
+
+const app = express()
+const PORT = process.env.PORT
+
+
+app.listen (PORT, () => console.log ("server is up and running on PORT:",PORT))
