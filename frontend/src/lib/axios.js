a140b4a3947020a3969b2a337980003f6533bdc0
Maruthi <palyammaaru14@gmail.com>
2026-08-07 16:50:17 +0530

    fix: remove unused Clerk token interceptor from axios instance

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,16 +1,6 @@
 import axios from "axios";
-import { clerk } from "@clerk/clerk-js";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-});
-
-axiosInstance.interceptors.request.use(async (config) => {
-  const token = await clerk.session?.getToken();
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
+  withCredentials: true,
 });
