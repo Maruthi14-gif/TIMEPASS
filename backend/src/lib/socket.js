e2747476baf6a7ee957de7d9a3f0f81f6896aa67
Maruthi <palyammaaru14@gmail.com>
2026-08-07 15:46:20 +0530

    fix: update socket server CORS configuration and adjust BASE_URL in useAuthStore

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -6,7 +6,12 @@ const app = express();
 const server = http.createServer(app);
 const allowedorigin = process.env.FRONTEND_URL || "http://localhost:5173";
 
-const io = new Server(server, {cors: {origin:[allowedorigin]}});
+const io = new Server(server, {
+  cors: {
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  },
+});
 
 function getReceiverSocketId(userId) {
     return userSocketMap[userId];
